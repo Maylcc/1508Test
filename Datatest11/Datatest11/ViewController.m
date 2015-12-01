b8fc93b588785b802772375c0b4f848e6d8a388e
123 <may_lcc@163.com>
2015-12-01 13:58:43 +0800

    git

---
+++ Datatest11/Datatest11/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Datatest11
+//
+//  Created by tarena on 15/12/1.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"福建登陆的手机发多少了");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
